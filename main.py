665a99f667ed751a7bb6584258e1bb4364d6de18
Aaron Mercado <mercadoaaron_bsit@plmun.edu.ph>
2026-03-02 21:27:34 +0800

    added Base.metadata.create_all(bind=engine) in main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from middleware.cors import add_cors_middleware
-
 from routes.role_route import router as role_router
+from database import engine, Base
 
 app = FastAPI()
 add_cors_middleware(app)
-
+Base.metadata.create_all(bind=engine)
 
 app.include_router(role_router, prefix="/api/role")
 
