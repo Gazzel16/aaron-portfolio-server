2ea8ee8a60a73fdcdeb1dff5708d008b4d70de6f
Aaron Mercado <mercadoaaron_bsit@plmun.edu.ph>
2026-03-02 19:30:17 +0800

    modify req.txt

--- middleware/cors.py
+++ middleware/cors.py
@@ -2,7 +2,7 @@ import os
 
 from fastapi.middleware.cors import CORSMiddleware
 
-_DEFAULT_ORIGINS = "http://localhost:8000, http://localhost:3000, http://127.0.0.1:8000"
+_DEFAULT_ORIGINS = "http://localhost:8000, http://localhost:3000, http://127.0.0.1:8000, http://0.0.0.0:8000 "
 
 
 def add_cors_middleware(app):
